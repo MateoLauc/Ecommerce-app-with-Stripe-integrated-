907b0788d25bcff3c720904943caa4523d4b3893
Ismir Mesetovic <ismir@atlantbh.com>
2016-11-02 00:06:54 +0100

    Filters fixed and partially refactored

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,8 @@ class CategoriesController < ApplicationController
   def show
       # Pronaći ID poslane kategorije i onda iz nje izabrati proizvode
     	@category= Category.find(params[:id])
-    	@variants= Variant.where(condition).order('price'+' '+ smjer).joins(:product).where(products: {category_id: @category.id})
+      query = QueryBuilder.build(params)
+    	@variants= Variant.where(query).order('price'+' '+ smjer).joins(:product).where(products: {category_id: @category.id})
     	#@variants=@variants.limit(16) opt
       @colors= Color.all
 
@@ -20,29 +21,53 @@ class CategoriesController < ApplicationController
 		  params[:direction] || "asc"
 		end 
     
-      def condition 
-           @cond=""
+      # def condition 
+      #      @cond=""
            
-           if params[:color_id] == nil 
-             @cond= ""
-           else 
-             @cond= "color_id = #{params[:color_id]}" 
-           end  
+      #      if params[:color_id] == nil 
+      #        @cond= ""
+      #      else 
+      #        @cond= "color_id = #{params[:color_id]}" 
+      #      end  
            
-           if params[:priceRange] != nil
-              @niz= []
-              @niz=params[:priceRange].split('-');
+      #      if params[:priceRange] != nil
+      #         @niz= []
+      #         @niz=params[:priceRange].split('-');
               
-              if @cond == ""
-                 @cond= "price >= #{@niz[0]} AND price <= @{@niz[1]}"
-              else
-                 @cond=@cond + " AND (price >= #{@niz[0]} AND price <= #{@niz[1]})"
-              end
+      #         if @cond == ""
+      #            @cond= "price >= #{@niz[0]} AND price <= @{@niz[1]}"
+      #         else
+      #            @cond=@cond + " AND (price >= #{@niz[0]} AND price <= #{@niz[1]})"
+      #         end
            
-           else   
-            @cond
-           end
+      #      else   
+      #       @cond
+      #      end
 
-      end 
+      # end 
+
+
+      class QueryBuilder
+        FILTERS = [:priceRange, :color_id]
+
+        def self.build(parameters)
+          query = []
+          FILTERS.each do |filter|
+            query << QueryBuilder.send(filter, parameters[filter])
+          end
+          query.join(' AND ')
+        end
+
+
+        def self.priceRange range
+          prices = range.split('-')
+          "price >= #{prices[0]} AND price <= #{prices[1]}"
+        end
+
+        def self.color_id color
+          "color_id = #{color}"
+        end
+
+      end
 end
 
